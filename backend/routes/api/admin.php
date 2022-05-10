8b24be2cd34175c6aab9cdff217a24c8da12076f
Anderson Vieira <anderson.vieira@way2.com.br>
2022-05-10 16:36:09 -0300

    teste routes and bug fix, create seed admin

--- backend/routes/api/admin.php
+++ backend/routes/api/admin.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\app\DomainController;
-use App\Http\Controllers\app\OnbordingController;
-use App\Http\Controllers\app\ProductController;
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function(){
 
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-    Route::middleware('auth:admin')->group(function(){
+    Route::middleware('auth:admins')->group(function(){
+        Route::get('/me', [AuthController::class, 'me'])->name('me');
 
     });
 
