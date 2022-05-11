eaa2917e87d8b9dcfdbedd2d2b9aa9dede27a7ea
Anderson Vieira <anderson.vieira@way2.com.br>
2022-05-10 21:49:56 -0300

    push point register

---
+++ frontend/components/ListPoint.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-white px-4 rounded-t shadow-lg dark:bg-gray-900  py-4">
+      <div class="mt-1 overflow-x-auto">
+          <h1 class="mb-3 text-xl text-center font-medium">Pontos Registrados Para o dia 01/01/2022</h1>
+
+          <div class="mb-3 flex items-center pl-5 focus:outline-none h-16 border border-gray-100 dark:border-gray-600  rounded">
+            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">
+              01/01/2022 22:23:10
+            </p>
+          </div>
+          <div class="mb-3 flex items-center pl-5 focus:outline-none h-16 border border-gray-100 dark:border-gray-600  rounded">
+            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">
+              01/01/2022 22:23:10
+            </p>
+          </div>
+          <div class="mb-3 flex items-center pl-5 focus:outline-none h-16 border border-gray-100 dark:border-gray-600  rounded">
+            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">
+              01/01/2022 22:23:10
+            </p>
+          </div>
+          <div class="mb-3 flex items-center pl-5 focus:outline-none h-16 border border-gray-100 dark:border-gray-600  rounded">
+            <p class="text-base font-medium leading-none text-gray-700 dark:text-white  mr-2">
+              01/01/2022 22:23:10
+            </p>
+          </div>
+
+      </div>
+  </div>
+
+</template>
+
+<!--
+<style lang="scss" scoped>
+    .checkbox:checked + .check-icon {
+        display: flex;
+    }
+</style>
+ -->
+
+
